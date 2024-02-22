a56c7e62fbcc4ec92fe781551d91be0081b26594
aravindm013 <aravindm013@gmail.com>
2024-02-22 13:18:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
+import axios from "axios"
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3055/api/tasks')
+      .then((response) => {
+        const result = response.data
+        setTasks(result)
+      })
+      .catch((err) => {
+        alert(err.message)
+      })
+  }, [] )
+  
+  const addTask = async (text, day, reminder) => {
+    const newTask = {
+      text: text,
+      day: day,
+      reminder: reminder
+    }
+    try {
+      await axios.post('http://localhost:3055/api/tasks', newTask)
+      const response = await axios.get('http://localhost:3055/api/tasks')
+      const result = response.data
+      setTasks(result)
+    } catch(err){
+      alert(err.message)
+    }
+  }
+  
+  const deleteTask = async (id) => {
+    try {
+      console.log("id",id)
+      const delResponse = await axios.delete(`http://localhost:3055/api/tasks/${id}`)
+      console.log(delResponse)
+      const response = await axios.get('http://localhost:3055/api/tasks')
+      const result = response.data
+      setTasks(result)
+    } catch(err){
+      console.log(err)
+      alert(err.message)
+    }
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder : !task.reminder} : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      { showAddTask && <AddTask onAdd={addTask} /> }
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask}  onToggle={toggleReminder} />
+        ) : (
+        'No tasks to show'
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App 
